f2aa21e03e8808bc3be37e189a8249df9143f832
siesie111 <szhang05@smith.edu>
2025-02-17 20:45:11 -0500

    updated hw3

--- Chatbot.java
+++ Chatbot.java
@@ -1,7 +1,10 @@
 interface Chatbot {
 
+    // run the chat, implement the functions to receive and answer chat from users
     void chat();
+    // print the transcript of the conversation
     void printTranscript();
+    // generate mirrored or canned responses base on the user input
     String respond(String inputString); 
     
 }
